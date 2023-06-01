7dac18444f67c159f2129ea9d80e5cedb69019a0
pajeon <80532545+pajeon@users.noreply.github.com>
2023-06-01 12:32:43 -0400

    Update

--- api/index.py
+++ api/index.py
@@ -15,7 +15,8 @@ ERC20_ABI = '[{"inputs":[{"internalType":"address","name":"account","type":"addr
 SDAO_ADDRESS = '0x04b269391Da04209d50aB4f4AD07a580f55E1840'
 SDAO_ABI = '[{"inputs":[],"name":"reservedLaunchTokens","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
 
-ENJIN_ADDRESS = '0xC77dC53421364EB0dFc52BAC616a6a014b9a23E9'
+ENJIN_ADDRESS_1 = '0xe8b75734Be6F138293849D6357350Ddb8c6Bfd14'
+ENJIN_ADDRESS_2 = '0x11A286b8cE9Ab6CdF93c4121E4C311CF2D91ffb1'
 ENJIN_ABI = '[{"inputs":[],"name":"totalReleasedAmount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]'
 
 w3 = Web3(Web3.HTTPProvider(BSC_RPC))
@@ -23,7 +24,8 @@ w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 cgv_contract = w3.eth.contract(address=CGV_ADDRESS, abi=ERC20_ABI)
 sdao_contract = w3.eth.contract(address=SDAO_ADDRESS, abi=SDAO_ABI)
-enjin_contract = w3.eth.contract(address=ENJIN_ADDRESS, abi=ENJIN_ABI)
+enjin_1_contract = w3.eth.contract(address=ENJIN_ADDRESS_1, abi=ENJIN_ABI)
+enjin_2_contract = w3.eth.contract(address=ENJIN_ADDRESS_2, abi=ENJIN_ABI)
 
 
 @app.route('/circulating_supply')
@@ -37,11 +39,13 @@ def circulating_supply():
     sdao_locked = sdao_contract.functions.reservedLaunchTokens().call() / 1e6
 
     enjin_locked = 6_000_000
-    enjin_claimed = enjin_contract.functions.totalReleasedAmount().call() / 1e6
+    enjin_1_claimed = enjin_1_contract.functions.totalReleasedAmount().call() / 1e6
+    enjin_2_claimed = enjin_2_contract.functions.totalReleasedAmount().call() / 1e6
 
     seedify_locked = 10_000_000
 
-    bsc_circulating = bsc_distributed - sdao_locked - enjin_locked + enjin_claimed - seedify_locked
+    bsc_circulating = bsc_distributed - sdao_locked - enjin_locked + \
+        enjin_1_claimed + enjin_2_claimed - seedify_locked
 
     total = bsc_circulating
 
